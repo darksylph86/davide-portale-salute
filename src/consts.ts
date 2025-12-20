f460060bb22840e8035e7873f0c090cdd97011a2
darksylph86 <darksylph86@gmail.com>
2025-12-20 13:20:40 +0100

    agg titolo e social

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Dr. Davide Ferraro';
+export const SITE_DESCRIPTION = 'Farmacista, Dottore in Nutrizione Umana, Esperto in Nutrizione Sportiva, Personal Trainer';
